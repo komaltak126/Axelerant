1fbc0297f0bb13c4cb3fb70be64409b6d4b55106
komaltak126 <55000950+komaltak126@users.noreply.github.com>
2019-09-07 01:01:11 +0530

    Create form.php

---
+++ form.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<style> body { background-color: lightblue }    </style>
+<form action="record.php" method="post">
+  <p>
+    <b>Who is your favourite Author?</b>
+  </p>
+<table>
+  
+     <tr><td><input type="radio" name="Author" value="Miguel de Cervantes">Miguel de Cervantes</td></tr>
+      <tr><td><input type="radio" name="Author" value="Charles Dickens">Charles Dickens</td></tr>
+      <tr><td><input type="radio" name="Author" value="J.R.R. Tolkien">J.R.R. Tolkien</td></tr>
+      <tr><td><input type="radio" name="Author" value="Antoine de Saint-Exuper">Antoine de Saint-Exuper</td></tr>
+      <tr><td><br><input type="submit" name="submit" value="submit"></td></tr>
+
+</table>  <br>
+</form>
+
+
+</body>
+</html>
